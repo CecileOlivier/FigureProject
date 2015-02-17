d830a380367d47cec3030d67af3c75d7a9b73023
Cecile <cecile.olivier56@gmail.com>
2015-02-17 17:48:48 +0100

    modification de l'url de la route oeuvres

--- figureprojectapp/urls.py
+++ figureprojectapp/urls.py
@@ -4,7 +4,7 @@ from figureprojectapp import views
 urlpatterns = patterns('',
     url(r'^/?$', views.home, name='home'),
     url(r'^biographie/', views.biographie, name='biographie'),
-    url(r'^oeuvres/', views.home, name='oeuvres'),
+    url(r'^#oeuvres/', views.home, name='oeuvres'),
     url(r'^oeuvre/(?P<slug>[a-z0-9-_]+)', views.oeuvre, name='oeuvre'),
     url(r'^extension-sauvage/', views.extensionsauvage, name='extensionsauvage'),
     url(r'^calendrier/', views.calendrier, name='calendrier'),
