3b450854f10f9e87e03916ce0b9fc7d5850c2794
Cecile <cecile.olivier56@gmail.com>
2015-02-21 15:48:41 +0100

    ajout des routes et des vues pour la version anglaise

--- figureprojectapp/views.py
+++ figureprojectapp/views.py
@@ -4,7 +4,7 @@ from figureprojectapp.models import Oeuvre, Biographie, Lien, Contact, Calendrie
 
 # Create your views here.
 # filter pour fr/en
-# variable, internationalisation doc django
+# variable, internationalisation
 
 def home(request):
     oeuvres = Oeuvre.objects.all()
@@ -13,7 +13,7 @@ def home(request):
 
 
 def oeuvre(request, slug):
-    objet_oeuvre = Oeuvre.objects.select_related().get(slug=slug)
+    objet_oeuvre = Oeuvre.objects.select_related().filter(langue="fr").get(slug=slug)
     img_oeuvre = objet_oeuvre.image_set.all()
     dates = objet_oeuvre.calendrier_set.all()
     next_oeuvre = Oeuvre.objects.filter(id__gt=objet_oeuvre.id).first()
@@ -22,7 +22,7 @@ def oeuvre(request, slug):
     return render(request, 'oeuvre.html', {'oeuvre': objet_oeuvre, 'img_oeuvre':img_oeuvre, 'dates':dates})
 
 def biographie(request):
-    objet_bio = Biographie.objects.get(titre='Formation/déformation')
+    objet_bio = Biographie.objects.filter(langue="en").get(titre='Formation/deformation')
     return render(request, 'biographie.html', {'formation': objet_bio})
 
 def extensionsauvage(request):
@@ -44,3 +44,40 @@ def liens(request):
     liens_artistes = Lien.objects.filter(domaine='Artistes')
     liens_utiles = Lien.objects.filter(domaine='Liens utiles')
     return render(request, 'liens.html', {'liens_partcult': liens_partcult, 'liens_parteduc': liens_parteduc, 'liens_artistes': liens_artistes, 'liens_utiles': liens_utiles})
+
+def works(request):
+    oeuvres = Oeuvre.objects.all()
+    return render(request, 'works.html', {'oeuvres': oeuvres})
+
+def work(request):
+    object_work = Oeuvre.objects.select_related().filter(langue="en").get(slug=slug)
+    img_oeuvre = objet_oeuvre.image_set.all()
+    dates = objet_oeuvre.calendrier_set.all()
+    next_oeuvre = Oeuvre.objects.filter(id__gt=objet_oeuvre.id).first()
+    prev_oeuvre = Oeuvre.objects.filter(id__gt=objet_oeuvre.id).first()
+#   1er slug : paramètre nomme, 2eme : variable fournie a def
+    return render(request, 'work.html', {'oeuvre': object_work, 'img_oeuvre':img_oeuvre, 'dates':dates})
+
+def links(request):
+    liens_partcult = Lien.objects.filter(domaine='Partenaires culturels')
+    liens_parteduc = Lien.objects.filter(domaine='Partenaires éducatifs')
+    liens_artistes = Lien.objects.filter(domaine='Artistes')
+    liens_utiles = Lien.objects.filter(domaine='Liens utiles')
+    return render(request, 'links.html', {'liens_partcult': liens_partcult, 'liens_parteduc': liens_parteduc, 'liens_artistes': liens_artistes, 'liens_utiles': liens_utiles})
+
+def biography(request):
+    objet_bio = Biographie.objects.filter(langue="en").get(titre='Formation/deformation')
+    return render(request, 'biography.html', {'formation': objet_bio})
+
+def extensionsauvageen(request):
+    extsauvage = Projet.objects.select_related('image').get(titre='Extension sauvage')
+    i_es = extsauvage.image_set.all()
+    return render(request, 'extension-sauvage-en.html', {'extsauvage': extsauvage, 'i_es': i_es})
+
+def calendar(request):
+    dates = Calendrier.objects.select_related('oeuvre').all()
+    return render(request, 'calendar.html', {'dates': dates})
+
+def contacten(request):
+    contacts = Contact.objects.all()
+    return render(request, 'contacten.html', {'contacts': contacts})
