e672e94675e0cdc72dc29186b6018f04d252a148
Cecile <cecile.olivier56@gmail.com>
2015-03-04 21:59:10 +0100

    ajout d'une classe actualité dans le model, l'admin et la récupération des objets dans la vue

--- figureprojectapp/admin.py
+++ figureprojectapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from figureprojectapp.models import Biographie, Calendrier, Contact, Oeuvre, Image, Lien, Langue, Projet, Atelier
+from figureprojectapp.models import Biographie, Calendrier, Contact, Oeuvre, Image, Lien, Langue, Projet, Atelier, Actualite
 
 # Register your models here.
 
@@ -14,12 +14,14 @@ class CalendrierAdmin(admin.ModelAdmin):
 class LienAdmin(admin.ModelAdmin):
     list_display = ('domaine', 'nom')
     search_fields = ['domaine', 'nom']
+    list_filter = ('domaine', 'nom')
 
 class ImageAdmin(admin.ModelAdmin):    
     # visuel c'est la fonction qui renvoi le code html qui va etre afficher 
     # faut aussi le mettre en 1er pour qu'il soit afficher comme lien
     list_display = ('visuel', 'image')
     search_fields = ['oeuvre', 'projet']
+    list_filter = ('oeuvre', 'projet')
 
 class OeuvreAdmin(admin.ModelAdmin):    
     list_display = ('titre_oeuvre', 'visuel', 'langue')
@@ -34,3 +36,4 @@ admin.site.register(Langue)
 admin.site.register(Oeuvre, OeuvreAdmin)
 admin.site.register(Projet)
 admin.site.register(Atelier)
+admin.site.register(Actualite)
--- figureprojectapp/views.py
+++ figureprojectapp/views.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
-from figureprojectapp.models import Oeuvre, Biographie, Lien, Contact, Calendrier, Projet, Image, Atelier
+from figureprojectapp.models import Oeuvre, Biographie, Lien, Contact, Calendrier, Projet, Image, Atelier, Actualite
 
 # Create your views here.
 # filter pour fr/en
@@ -11,15 +11,15 @@ def oeuvres(request):
     return render(request, 'index.html', {'oeuvres': oeuvres})
     # objet request + template + contexte cad les variables qui seront disponibles dans le template
 
-
 def oeuvre(request, slug):
+    oeuvres = Oeuvre.objects.filter(langue='fr').all()
     objet_oeuvre = Oeuvre.objects.select_related().filter(langue="fr").get(slug=slug)
     img_oeuvre = objet_oeuvre.image_set.all()
     dates = objet_oeuvre.calendrier_set.all()
     next_oeuvre = Oeuvre.objects.filter(id__gt=objet_oeuvre.id).first()
     prev_oeuvre = Oeuvre.objects.filter(id__gt=objet_oeuvre.id).first()
 #   1er slug : paramètre nomme, 2eme : variable fournie a def
-    return render(request, 'oeuvre.html', {'oeuvre': objet_oeuvre, 'img_oeuvre':img_oeuvre, 'dates':dates, 'next': next_oeuvre, 'prev': prev_oeuvre})
+    return render(request, 'oeuvre.html', {'oeuvre': objet_oeuvre, 'img_oeuvre':img_oeuvre, 'dates':dates, 'next': next_oeuvre, 'prev': prev_oeuvre, 'oeuvres': oeuvres})
 
 def biographie(request):
     biographie = Biographie.objects.filter(langue='fr').all()
@@ -50,6 +50,10 @@ def liens(request):
     liens_artistes = Lien.objects.filter(domaine='Artistes')
     liens_utiles = Lien.objects.filter(domaine='Liens utiles')
     return render(request, 'liens.html', {'liens_partcult': liens_partcult, 'liens_parteduc': liens_parteduc, 'liens_artistes': liens_artistes, 'liens_utiles': liens_utiles})
+
+def actualite(request):
+    actu = Actualite.objects.filter(langue='fr').all()
+    return render(request, 'actu.html', {'actu': actu})
 
 def works(request):
     oeuvres = Oeuvre.objects.filter(langue='en').all()
