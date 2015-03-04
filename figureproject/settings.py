3799dcb8f7669b50e7a778bff2d442a910b8aa98
Cecile <cecile.olivier56@gmail.com>
2015-03-04 20:09:32 +0100

    configuration minimale de MEDIA_*

--- figureproject/settings.py
+++ figureproject/settings.py
@@ -84,7 +84,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+STATIC_ROOT = ''
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'medias')
+MEDIA_URL = '/media/'
 
 try:
     from localsettings import *
