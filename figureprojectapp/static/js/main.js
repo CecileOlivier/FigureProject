9adc39a74d848e2cfccd979fadb3ae6264a16721
Cecile <cecile.olivier56@gmail.com>
2015-03-29 18:51:19 +0200

    gestion des liens actifs dans oeuvre et liens + affichage de bloc ou non selon les medias queries

--- figureprojectapp/static/js/main.js
+++ figureprojectapp/static/js/main.js
@@ -2,16 +2,19 @@ $(document).ready(function() {
 	$(".lienvisible").click(function(event) {
 		$(event.currentTarget).show();
 	    var val_link = $(event.currentTarget).attr('href'); 
-	    console.log(val_link);
+	    console.log('clic sur '+val_link);
 	    $(val_link).toggle();
+	    $("a h2").css("color", "#3D3D3F");
+	    $("a[href="+val_link+"] > h2").css("color", "#E1001A");
+	    console.log("a[href="+val_link+"]");
 	});
-	// si un hash est présent, on simule le clic sur le lien correspondant
+	/* simulation du clic */
     var hash = window.location.hash;
     if(hash) {
         var lien = $('[href="'+hash+'"]')
-        console.log(lien);
         lien.click();
     }
+    /* media queries */
 	if ($(window).width() < 360) {
 		console.log('petit');
 		$(".hide").hide(); 
@@ -27,22 +30,22 @@ $(document).ready(function() {
 		// jquery.mousewheel.min.js pour le scroll horizontal
 		$('html, body, *').on('mousewheel', function(e, delta) {
 			this.scrollLeft -= (delta * 40);
+			console.log('je fais qqchose');
 			e.preventDefault();
 		});
 		// custom scrollbar
-		/*$("#note").mCustomScrollbar({
+		$("#note").mCustomScrollbar({
     		theme:"rounded-dark",
     		scrollButtons:{
-     		 	enable: true // on choisit d'afficher les flèches haut et bas
+     		 	enable: true // affichage flèches haut et bas
     		}
-		});*/
+		});
 		$("#note").removeClass("hide");
 		$("#menu-principal").removeClass("hide");
 		$("#oeuvres").removeClass("hide");
 		$("#calendrier").removeClass("hide");
 		$("#diffusion").addClass("hide");
-		$("a[href=#note]").css("display:none;");
+		$("a[href=#note] > h2").css("display", "none");
 		$(".hide").hide(); 
 	}
-	/* tester avec un if et un else, puis un autre if */
 });
