55b8ded54066abc3e6e2c723746c65a2e058d6ba
Cecile <cecile.olivier56@gmail.com>
2015-03-29 17:26:04 +0200

    ajout de summernote dans le fichier url du projet

--- figureproject/urls.py
+++ figureproject/urls.py
@@ -1,5 +1,6 @@
-from django.conf.urls import patterns, include, url
+from django.conf.urls import patterns, include, url, static
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = patterns('',
     # Examples:
@@ -10,5 +11,5 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     # url(r'^$', 'figureproject.views.home', name='home'),
     url(r'^', include('figureprojectapp.urls')),
-
-)
+    url(r'^summernote/', include('django_summernote.urls')),
+) + static.static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
